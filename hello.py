6591ea48204d785282a955ebaae1669f12143da8
Haseeb <haseeburrehman1098@gmail.com>
2025-10-07 04:59:39 +0500

    Add hello.py Python script for GitHub release

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Hello GitHub Release Script
+Version: 1.0.0
+"""
+
+def main():
+    print("Hello, GitHub Release!")
+    print("This is version 1.0.0 of our Python script!")
+
+if __name__ == "__main__":
+    main()
